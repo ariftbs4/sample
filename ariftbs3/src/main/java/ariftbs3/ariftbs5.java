30018353d842cb05b6f0df298e0db205b73a0a45
Md Hosen <mdhosen@mds-macbook-pro.lan>
2023-02-06 18:55:15 -0500

    add new project

---
+++ ariftbs3/src/main/java/ariftbs3/ariftbs5.java
@@ -0,0 +1,10 @@
+package ariftbs3;
+
+public class ariftbs5 {
+	<!-- https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java -->
+		<dependency>
+		    <groupId>org.seleniumhq.selenium</groupId>
+		    <artifactId>selenium-java</artifactId>
+		    <version>4.8.0</version>
+		</dependency>
+}
